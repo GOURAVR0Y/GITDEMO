f4b2936548738bc4bbe943353e2097b299631e61
GOURAV ROY <roy,pushki29@gmail.com>
2022-07-29 01:39:00 +0530

    2nd commit

--- src/main/java/BaseUtils/EventBastUtils.java
+++ src/main/java/BaseUtils/EventBastUtils.java
@@ -4,6 +4,8 @@ public class EventBastUtils {
 
 	public static void main(String[] args) {
 		System.out.println(System.getProperty("user.dir"));
+		System.out.println(System.getProperty("user.dir"));
+		System.out.println(System.getProperty("user.dir"));
 
 	}
 
